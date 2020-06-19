a6a0e90781370c72eaa18cb4de81496a5bc85823
sample2 <niaoyuexingshu@gmail.com>
2020-06-19 14:48:11 +0900

    [追加]簡単ログインの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new_guest
+    user = User.find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    end
+  sign_in user
+  end
 end
