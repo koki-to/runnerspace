a68bde1f7b126e8b2796023d262926c8f7f6948b
sample2 <niaoyuexingshu@gmail.com>
2020-06-21 14:48:19 +0900

    [調整]tweets.indexの調整

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 12 }
+  validates :title, presence: true, length: { maximum: 10 }
   validates :text, presence: true, length: { maximum: 200 }
   belongs_to :user
   has_many :comments, dependent: :destroy
