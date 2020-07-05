87035666776d1064853305dde5f3f8e7dae27219
sample2 <niaoyuexingshu@gmail.com>
2020-07-05 18:49:52 +0900

    [調整]本番環境の実装

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,4 +1,5 @@
 class RanksController < ApplicationController
+  before_action :authenticate_user!
   def index
     @all_rank = User.joins(:tweets).group(:user_id).order('sum(run) desc').limit(10)
   end
